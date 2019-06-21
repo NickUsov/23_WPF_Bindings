4f9732d652b5bd239c89d690512fa2948d0c40f9
NickUsov <nicky.yalta.ua@gmail.com>
2019-06-21 21:51:41 +0300

    XAML&C#

---
+++ 23_WPF_Bindings/23_WPF_Bindings/Employee.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _23_WPF_Bindings
+{
+    public class Employee : INotifyPropertyChanged
+    {
+        string sirname;
+        int age;
+        string rank;
+        public string Sirname { get { return sirname; } set { sirname = value; OnPropertyChanged("Name"); } }
+        public int Age { get { return age; } set { age = value; OnPropertyChanged("Age"); } }
+        public string Rank { get { return rank; } set { rank = value; OnPropertyChanged("Rank"); } }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName]string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
---
+++ 23_WPF_Bindings/23_WPF_Bindings/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _23_WPF_Bindings
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Employee emp = new Employee { Sirname = "Anton", Age = 22, Rank = "manager" };
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = emp;
+        }
+
+        private void bt1_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show($"Фамилия {emp.Sirname} возраст {emp.Age} должность {emp.Rank}");
+        }
+    }
+}
